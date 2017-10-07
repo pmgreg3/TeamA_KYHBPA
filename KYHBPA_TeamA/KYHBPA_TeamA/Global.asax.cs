5a989c5c5ed473410358ba14d22281501cd564e2
peter <pmgreg3@gmail.com>
2017-10-07 10:27:52 -0400

    adding 404 page

--- KYHBPA_TeamA/KYHBPA_TeamA/Global.asax.cs
+++ KYHBPA_TeamA/KYHBPA_TeamA/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KYHBPA_TeamA.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +17,34 @@ namespace KYHBPA_TeamA
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+        protected void Application_EndRequest()
+        {
+            if (Context.Response.StatusCode == 404)
+            {
+                Response.Clear();
+
+                var errorRoute = new RouteData();
+                errorRoute.Values["controller"] = "Error";
+                errorRoute.Values["action"] = "ErrorPage";
+
+                IController c = new ErrorController();
+                c.Execute(new RequestContext(new HttpContextWrapper(Context), errorRoute));
+            }
+            // TODO: IMPLEMENT 500 > ERROR PAGE
+            //else if (Context.Response.StatusCode >= 500)
+            //{
+            //    Response.Clear();
+
+            //    var errorRoute = new RouteData();
+            //    errorRoute.Values["controller"] = "Error";
+            //    errorRoute.Values["action"] = "FiveOhError";
+
+            //    IController c = new ErrorController();
+            //    c.Execute(new RequestContext(new HttpContextWrapper(Context), errorRoute));
+            //}
         }
     }
 }
