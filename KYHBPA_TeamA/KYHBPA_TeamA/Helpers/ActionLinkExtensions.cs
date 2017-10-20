51345b5d84b33be14e805d5074239cd200e67f20
Dylan T Thomas <dtthom09@louisville.edu>
2017-10-20 18:11:32 -0400

    Update

---
+++ KYHBPA_TeamA/KYHBPA_TeamA/Helpers/ActionLinkExtensions.cs
@@ -0,0 +1,47 @@
+﻿using KYHBPA_TeamA.Models;
+using System;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace KYHBPA_TeamA.Helpers
+{
+    public static class ActionLinkExtensions
+    {
+        public static MvcHtmlString PostLink(this HtmlHelper helper, Post post)
+        {
+            return helper.ActionLink(post.Title, "Post", "Blog",
+                new
+                {
+                    year = post.PostedOn.Year,
+                    month = post.PostedOn.Month,
+                    title = post.UrlSlug
+                },
+                new
+                {
+                    title = post.Title
+                });
+        }
+
+        public static MvcHtmlString CategoryLink(this HtmlHelper helper,Category category)
+        {
+            return helper.ActionLink(category.Name, "ViewByCategory", "Blog",
+                new
+                {
+                    Id = category.Id
+                },
+                new
+                {
+                    title = String.Format("See all {0} posts", category.Name)
+                });
+        }
+
+        public static MvcHtmlString TagLink(this HtmlHelper helper, Tag tag)
+        {
+            return helper.ActionLink(tag.Name, "Tag", "Blog", new { tag = tag.UrlSlug },
+                new
+                {
+                    title = String.Format("See all posts in {0}", tag.Name)
+                });
+        }
+    }
+}
