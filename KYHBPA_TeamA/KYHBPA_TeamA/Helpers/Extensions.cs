dc41614d516e0bc6609ba3f43c27a226c13d4b29
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2017-11-17 18:17:07 -0500

    Fixed the time on the news and blog

--- KYHBPA_TeamA/KYHBPA_TeamA/Helpers/Extensions.cs
+++ KYHBPA_TeamA/KYHBPA_TeamA/Helpers/Extensions.cs
@@ -1,18 +1,34 @@
-﻿using System;
+﻿using KYHBPA_TeamA.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace KYHBPA_TeamA.Helpers
 {
     public static class Extensions
     {
-        public static string ToConfigLocalTime(this DateTime utcDT)
-        {    
-            
-            var istTZ = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");//ConfigurationManager.AppSettings["Timezone"]);
-            return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
-        }
+
+            public static string ToConfigLocalTime(this DateTime utcDT)
+            {
+                var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+                return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc
+                    (utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
+            }
+
+            public static string Href(this Post post, UrlHelper helper)
+            {
+                return helper.RouteUrl(new
+                {
+                    controller = "Blog",
+                    action = "Post",
+                    year = post.PostedOn.Year,
+                    month = post.PostedOn.Month,
+                    title = post.UrlSlug
+                });
+
+            }
     }
 }
