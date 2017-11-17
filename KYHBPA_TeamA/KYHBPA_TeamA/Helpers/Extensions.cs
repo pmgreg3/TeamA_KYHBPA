484c15a0a97b3b375716854191db6ec839aec9d0
Dylan T Thomas <dtthom09@cardmail.louisville.edu>
2017-11-17 17:30:35 -0500

    .

--- KYHBPA_TeamA/KYHBPA_TeamA/Helpers/Extensions.cs
+++ KYHBPA_TeamA/KYHBPA_TeamA/Helpers/Extensions.cs
@@ -9,8 +9,8 @@ namespace KYHBPA_TeamA.Helpers
     public static class Extensions
     {
         public static string ToConfigLocalTime(this DateTime utcDT)
-        {
-
+        {    
+            
             var istTZ = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");//ConfigurationManager.AppSettings["Timezone"]);
             return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
         }
