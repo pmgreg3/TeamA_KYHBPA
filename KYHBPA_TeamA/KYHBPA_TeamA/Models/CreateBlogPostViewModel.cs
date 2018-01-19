a9b23b0201fa64d1c389478475dbb875fdb115ef
peter <pmgreg3@gmail.com>
2018-01-19 17:20:13 +0000

    adding display name for attribute to shortDescription property

--- KYHBPA_TeamA/KYHBPA_TeamA/Models/CreateBlogPostViewModel.cs
+++ KYHBPA_TeamA/KYHBPA_TeamA/Models/CreateBlogPostViewModel.cs
@@ -73,6 +73,7 @@ namespace KYHBPA_TeamA.Models
 
         public string Title { get; set; }
 
+        [Display(Name = "Short Description")]
         public string ShortDescription { get; set; }
 
         public string Description { get; set; }
