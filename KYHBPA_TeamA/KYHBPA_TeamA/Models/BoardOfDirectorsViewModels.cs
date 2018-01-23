fa54e83c3e77757eee5dcb2550d9c2db915a0f71
peter <pmgreg3@gmail.com>
2018-01-23 11:38:44 +0000

    added ordering for BoD. rerouted admins back to admin page for BoD Controller. enlarged description text boxes

--- KYHBPA_TeamA/KYHBPA_TeamA/Models/BoardOfDirectorsViewModels.cs
+++ KYHBPA_TeamA/KYHBPA_TeamA/Models/BoardOfDirectorsViewModels.cs
@@ -5,6 +5,7 @@ using System.Web;
 
 namespace KYHBPA_TeamA.Models
 {
+
     public class AddBoardofDirectorsViewModel
     {
         public string Title { get; set; }
@@ -13,12 +14,54 @@ namespace KYHBPA_TeamA.Models
     }
     public class DisplayBoardOfDirectorsViewModel
     {
+        private int _Ranking;
+        private string _Title;
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Title { get; set; }
+        public string Title
+        {
+            get
+            {
+                return _Title;
+            }
+
+            set
+            {
+                _Title = value;
+            }
+        }
         public string Email { get; set; }
         public string Description { get; set; }
         public byte[] PhotoContent { get; set; }
+        public int Rank
+        {
+            get
+            {
+                return _Ranking;
+            }
+            set
+            {
+                if (_Title == "President")
+                    _Ranking = 1;
+                else if (_Title == "Vice President")
+                    _Ranking = 2;
+                else if (_Title == "Executive Director")
+                    _Ranking = 3;
+                else if (_Title == "Treasurer")
+                    _Ranking = 4;
+                else if (_Title == "Executive Assistant")
+                    _Ranking = 5;
+                else if (_Title == "Owner Director")
+                    _Ranking = 6;
+                else if (_Title == "Trainer Director")
+                    _Ranking = 7;
+                else if (_Title == "Alternate Trainer Director")
+                    _Ranking = 8;
+                else
+                    _Ranking = 9;
+            }
+        }
     }
 }
