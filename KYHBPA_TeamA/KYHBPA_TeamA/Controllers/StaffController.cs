a37770a72c0f980067cd0ece211f3e407dd84fbe
peter <pmgreg3@gmail.com>
2018-02-05 09:51:32 +0000

    adding staffing controller

---
+++ KYHBPA_TeamA/KYHBPA_TeamA/Controllers/StaffController.cs
@@ -0,0 +1,98 @@
+﻿using KYHBPA_TeamA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KYHBPA_TeamA.Controllers
+{
+    public class StaffController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+
+        // Admin of Staff
+        public ActionResult Admin()
+        {
+            return View();
+        }
+
+        // GET: Staff
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Staff/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Staff/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Staff/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Staff/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Staff/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Staff/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Staff/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
