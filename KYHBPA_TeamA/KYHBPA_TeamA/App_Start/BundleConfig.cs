489795f1c1265ae72958d43286f0c04ea0ded714
peter <pmgreg3@gmail.com>
2018-02-17 09:57:44 +0000

    bundling event card js.  fixing publisher data.

--- KYHBPA_TeamA/KYHBPA_TeamA/App_Start/BundleConfig.cs
+++ KYHBPA_TeamA/KYHBPA_TeamA/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace KYHBPA_TeamA
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js").Include(
+                        "~/Scripts/EventCard.js"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ KYHBPA_TeamA/KYHBPA_TeamA/Scripts/EventCard.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    var eventButtonClicked = false;
+
+    $('.collapsible').click(function () {
+        var itemID = $(this).attr('data-target');
+        var targetElement = $(itemID);
+        var eventHasBeenClicked = targetElement.attr('class');
+
+        if (eventHasBeenClicked != "in") {
+            $(this).empty().append("<i style='color:#072419' data-toggle='collapse' data-target='" + itemID + "' class='fa fa-angle-up fa-2x'></i>");
+            eventButtonClicked = true;
+        } else {
+            $(this).empty().append("<i style='color:#072419' data-toggle='collapse' data-target='" + itemID + "' class='fa fa-angle-down fa-2x'></i>");
+            eventButtonClicked = false;
+        }
+    });
+});
